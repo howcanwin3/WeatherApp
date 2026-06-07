ff90bc3a1caab03415f27d5d9840e270edf25004
2802 <itcatdemo@163.com>
2026-06-07 18:09:41 +0800

    1.定义Dto接数据 2.造翻译官mapper->转换接收的数据

--- app/src/main/java/com/example/weatherforecastapp/data/remote/model/WeatherDto.kt
+++ app/src/main/java/com/example/weatherforecastapp/data/remote/model/WeatherDto.kt
@@ -1,4 +1,18 @@
 package com.example.weatherforecastapp.data.remote.model
 
-class WeatherDto {
-}
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+//Serializable数据类-->用于接收服务器返回的JSON数据 value = "code" code是服务器返回的字段
+@Serializable
+data class WeatherDto (
+    @SerializedName("code") val code : String,
+    @SerializedName(value = "daily") val dailyList : List<DailyDto>//新建一个属性dailyList-->新建一个data class 用于指定列表数据类型
+    )
+
+@Serializable
+data class DailyDto(
+    @SerializedName("fxDate") val fxDate : String,
+    @SerializedName("tempMax") val tempMax : String,
+    @SerializedName("tempMin") val tempMin : String,
+    @SerializedName("textDay") val textDay : String,
+)
---
+++ app/src/main/java/com/example/weatherforecastapp/ui/mapper/WeatherMapper.kt
@@ -0,0 +1,24 @@
+package com.example.weatherforecastapp.ui.mapper
+
+import com.example.weatherforecastapp.data.remote.model.WeatherDto
+import com.example.weatherforecastapp.ui.screen.ForecastItem
+import com.example.weatherforecastapp.ui.screen.WeatherUiState
+
+fun WeatherDto.toWeatherUiState(cityName : String = "上海") : WeatherUiState {
+
+    val today = this.dailyList.firstOrNull()//由于Dto拿到的是一个List->拿到列表中第一个元素
+    //返回一个WeatherUiState对象
+    return WeatherUiState(
+        cityName = cityName,
+        currentTemperature = "${today?.tempMax?:"0"}",
+        weatherDescription = "${today?.textDay?:"--"}",
+        forecastItems = this.dailyList.map{         //list的扩展函数map 遍历列表将箭头左边的类映射到右边
+            dailyDto ->ForecastItem(
+            dayOfWeek = dailyDto.fxDate,
+            weather = dailyDto.textDay,
+            temperatureRange = "${dailyDto.tempMin}-----${dailyDto.tempMax}"
+            )
+        }
+    )
+}
+
